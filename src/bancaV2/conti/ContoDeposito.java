0aa4091e859c3a4fad999a3fa728f820b6bd5a25
enea <enea.parimbelli@gmail.com>
2015-11-05 12:40:25 +0100

    Further refactoring to remove AbstractConto (was overlapping with ContoCorrente)

--- src/bancaV2/conti/ContoDeposito.java
+++ src/bancaV2/conti/ContoDeposito.java
@@ -1,9 +1,9 @@
 package bancaV2.conti;
 
 import bancaV2.ContoType;
-import bancaV2.conti.AbstractConto;
+import bancaV2.conti.ContoCorrente;
 
-public class ContoDeposito extends AbstractConto {
+public class ContoDeposito extends ContoCorrente {
 
     public ContoDeposito(String iban, String cf, double saldo) {
         super(iban, cf, 0.0);
